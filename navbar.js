ced99de964cc3a37c74d4090f2290242bdc24b30
Oravanya-S <oravanya34905@gmail.com>
2023-04-03 23:12:34 +0700

    finish project

---
+++ navbar.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "350px";
+
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
